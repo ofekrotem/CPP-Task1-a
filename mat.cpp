1001603940d74d66d276282393d284a0382f1b6b
Ofek Rotem <92383710+ofekrotem@users.noreply.github.com>
2022-03-09 15:52:34 +0200

    Fixed tidy

--- mat.cpp
+++ mat.cpp
@@ -10,9 +10,9 @@
 using namespace std;
 namespace ariel {
     string mat(int col, int row, char firstSym, char secondSym) {
-        if (col %2 == 0 || row%2==0) __throw_invalid_argument("Enter only odd row and column size");
-        if (firstSym == secondSym) __throw_invalid_argument("Enter different symbols");
-        if (col<=0 || row<=0) __throw_invalid_argument("Enter positive row and column size");
+        if (col %2 == 0 || row%2==0) {__throw_invalid_argument("Enter only odd row and column size");}
+        if (firstSym == secondSym) {__throw_invalid_argument("Enter different symbols");}
+        if (col<=0 || row<=0) {__throw_invalid_argument("Enter positive row and column size");}
         return "Matala shel 100 lefahot";
     }
 
